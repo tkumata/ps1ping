b14185d676af8138fc19c123fd7c4f2aad1ff18a
Tomokatsu Kumata <tkumata@gmail.com>
2016-02-23 01:41:47 +0900

    function 化

--- ps1ping.ps1
+++ ps1ping.ps1
@@ -4,29 +4,38 @@
  #     ps1ping.ps1 -servers www.google.co.jp,192.168.2.1
  #
 #>
-param (
-    [string]$inputpath,
-    [string[]]$Servers,
-    [string]$outputpath = "C:\temp\pingresult.html"
-)
 
-if ($inputpath) {
-    $pcname = Get-Content $inputpath
-} elseif ($Servers) {
-    $pcname = $Servers
-} else {
-    Write-Warning "Please Specify Params"
-    exit
+# param (
+#     [string]$inputpath,
+#     [string[]]$Servers,
+#     [string]$outputpath = "C:\temp\pingresult.html"
+# )
+
+# if ($inputpath) {
+#     $pcname = Get-Content $inputpath
+# } elseif ($Servers) {
+#     $pcname = $Servers
+# } else {
+#     echo "Please Specify Params"
+#     exit
+# }
+
+function Main()
+{
+    $pcname = @("www.google.com","192.168.2.1")
+    $outputpath = "C:\temp\pingresult.html"
+    $interval = 300
+    $head = "<meta http-equiv='refresh' content='$interval' />"
+    
+    while (1) {
+        $isalive = @(Test-Connection -ComputerName $pcname -Count 1 -Quiet)
+        $result = 0..($pcname.Count - 1) | %{$pcname[$_] + "," + $isalive[$_]}
+        $body = "<h1>Check Hosts Aliveness</h1><h2>Check at " + (Get-Date).ToString() + "</h2>"
+        $html = $result | ConvertFrom-Csv -Header "Host Name","isAlive" | ConvertTo-Html -Head $head -Title "Hosts" -Body $body
+        $html | Out-File $outputpath
+        sleep -s $interval
+    }
 }
 
-$interval = 600
-$head = "<meta http-equiv='refresh' content='$interval' />"
-
-while (1) {
-    $isalive = @(Test-Connection -ComputerName $pcname -Count 1 -Quiet)
-    $result = 0..($pcname.Count - 1) | %{$pcname[$_] + "," + $isalive[$_]}
-    $body = "<h1>Check Hosts Aliveness</h1><h2>Check at " + (Get-Date).ToString() + "</h2>"
-    $html = $result | ConvertFrom-Csv -Header "Host Name","isAlive" | ConvertTo-Html -Head $head -Title "Hosts" -Body $body
-    $html | Out-File $outputpath
-    sleep -s $interval
-}
+# New-SelfHostedPS ps1ping.ps -Service -ServiceName ps1ping -ServiceDisplayName "ps1ping Demo"
+Main
