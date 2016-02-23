740aa9fc86835d7b5a1dced8c1fae1f464f71b59
Tomokatsu Kumata <tkumata@gmail.com>
2016-02-24 00:30:34 +0900

    style 追加。日本語はどーしても文字化けするので諦めた。

--- ps1ping.ps1
+++ ps1ping.ps1
@@ -12,8 +12,7 @@ param (
     [string]$outputpath = "C:\temp\pingresult.html"
 )
 
-function Main()
-{
+function Main() {
     if ($inputfile) {
         $pcname = Get-Content $inputfile
     } elseif ($servers) {
@@ -25,18 +24,29 @@ function Main()
     
     $interval = 1800
     $refresh = $interval + 60
-    $head = "<meta http-equiv='refresh' content='$refresh' />"
+    $head = "<meta charset=`"UTF-8`">
+<meta http-equiv=`"refresh`" content=`"$refresh`" />
+<style>
+* {font-family:arial;}
+h1 {font-size:32px;font-weight:bold;}
+h2 {font-size:24px;font-weight:bold;}
+th {font-weight:normal;color:#fff;background:#14a;width:200px;text-align:center;border-bottom:1px solid #ccc;}
+table tr:nth-child(2n+1) {background:#f1f6fc;}
+.msg {margin:10px 0 10px 0;}
+</style>"
     
     while (1) {
         $isalive = @(Test-Connection -ComputerName $pcname -Count 1 -Quiet)
         $result = 0..($pcname.Count - 1) | %{$pcname[$_] + "," + $isalive[$_]}
         $body = "<h1>Check Hosts Aliveness</h1>`n"
-        $body += "<h2>Check at " + (Get-Date).ToString() + "</h2>`n"
+        $body += $messText
+        $body += "<div class=`"msg`">Check at " + (Get-Date).ToString() + "</div>`n"
         $html = $result | ConvertFrom-Csv -Header "Host Name","isAlive" | ConvertTo-Html -Head $head -Title "Hosts" -Body $body
-        $html | Out-File $outputpath
+        $html | Out-File $outputpath -Encoding UTF8
         sleep -s $interval
     }
 }
 
 # New-SelfHostedPS ps1ping.ps -Service -ServiceName ps1ping -ServiceDisplayName "ps1ping Demo"
+$messText = "<h2>ping result</h2>`n"
 Main
